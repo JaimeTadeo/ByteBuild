3547f03e85134f57d74e99b83613cc3a21dfd5b9
Diego Lazarte <ogeilazarter@gmail.com>
2025-03-27 18:26:43 -0400

    Arreglo de errror img nvidia y implementación completa de card

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router'; // Asegúrate de importar RouterOutlet
 import { InicioComponent } from './inicio/inicio.component'; // Asegúrate de importar InicioComponent
+import {CardFancyComponent} from './card-fancy/card-fancy.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [InicioComponent]  
+  imports: [InicioComponent, CardFancyComponent]  
 })
 export class AppComponent {
   title = 'TuProyecto';
--- FrontEnd/src/app/card-fancy/card-fancy.component.ts
+++ FrontEnd/src/app/card-fancy/card-fancy.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-card-fancy',
-  imports: [],
+  imports: [MatButtonModule,MatCardModule],
   templateUrl: './card-fancy.component.html',
-  styleUrl: './card-fancy.component.css'
+  styleUrl: './card-fancy.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CardFancyComponent {
-
-}
+export class CardFancyComponent {}
