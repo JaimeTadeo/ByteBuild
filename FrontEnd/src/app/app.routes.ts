8b70b1f2d7270022763a87fd528fe58dbd2906ff
JaimeTadeo <deugartebutron@gmail.com>
2025-03-31 13:37:19 -0400

    paginas nuevas del frotn

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { ComparacionComponent } from './comparacion/comparacion.component';
+import { TrackingComponent } from './tracking/tracking.component'; // Añade esta línea
 
 export const routes: Routes = [
-  { path: '', component: InicioComponent }
+  { path: '', component: InicioComponent },
+  { path: 'comparacion', component: ComparacionComponent },
+  { path: 'tracking', component: TrackingComponent }
 ];
