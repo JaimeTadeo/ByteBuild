023f087fd9781927a9897b34393526ab04ac470f
Jose <jose.lopez.s@ucb.edu.bo>
2025-04-03 08:53:04 -0400

    Fix: se fixearon los datos para tener apellido en la database

--- backend/src/main/java/com/bytebuilder/backend/usuarios/Usuario.java
+++ backend/src/main/java/com/bytebuilder/backend/usuarios/Usuario.java
@@ -9,15 +9,17 @@ public class Usuario {
     private String id;
     private int idUsuario;
     private String nombre;
+    private String apellido;
     private String correo;
     private String password;
     private String rol; // "usuario" o "admin"
 
     public Usuario() {}
 
-    public Usuario(int idUsuario, String nombre, String correo, String password, String rol) {
+    public Usuario(int idUsuario, String nombre, String apellido,String correo, String password, String rol) {
         this.idUsuario = idUsuario;
         this.nombre = nombre;
+        this.apellido = apellido;
         this.correo = correo;
         this.password = password;
         this.rol = rol;
@@ -29,6 +31,8 @@ public class Usuario {
     public void setIdUsuario(int idUsuario) { this.idUsuario = idUsuario; }
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getApellido() { return apellido; }
+    public void setApellido(String apellido) { this.apellido = apellido; }
     public String getCorreo() { return correo; }
     public void setCorreo(String correo) { this.correo = correo; }
     public String getPassword() { return password; }
