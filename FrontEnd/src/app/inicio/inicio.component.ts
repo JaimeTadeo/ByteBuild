f94305094acca09dc5ae9ff4e0af03e29a90c26e
Diego Lazarte <ogeilazarter@gmail.com>
2025-04-15 15:53:52 -0400

    Colocar el componente header

--- FrontEnd/src/app/inicio/inicio.component.ts
+++ FrontEnd/src/app/inicio/inicio.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [NavbarComponent], 
+  imports: [NavbarComponent,HeaderComponent], 
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
